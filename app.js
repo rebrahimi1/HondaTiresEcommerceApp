8eeb286755207d4a37fe4e38f6dcaf0d7f8cd0dd
Reza <reebrah@gmail.com>
2021-09-11 14:22:00 -0400

    Second Commit

--- app.js
+++ app.js
@@ -515,7 +515,7 @@ app.get("/contact", function(req, res){
 
 
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("Server started successfully");
 });
 
